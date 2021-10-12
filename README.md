d82f01fbf72d32f853a8e6fe2a81049b8548e0cc
Ion <icaliman92@gmail.com>
2021-10-12 12:18:37 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# _s theme generator
+
+
+Hi. Want to develop a WordPress theme and are looking for a good starter theme? That's what I'm here for. I'm a generator for the starter theme called `_s`, or `underscores`, if you like.
+
+Installation
+---------------
+
+### Requirements
+
+`_s` requires the following dependencies:
+
+- [Node.js](https://nodejs.org/)
+- [Composer](https://getcomposer.org/)
+
+### Quick Start
+
+#### 1 - Generate
+Open the Terminal inside the `themes` directory and run the folowing commands:
+
+```sh
+$ cd /path/to/wp-content/themes
+$ npx _s
+```
+
+You will be prompted to enter the theme name, slug and path.
+
+#### 2 - Setup
+To start using all the tools that come with `_s`  you need to install the necessary Node.js and Composer dependencies:
+
+```sh
+$ cd theme-dir
+$ composer install
+$ npm install
+```
+
+### Available CLI commands
+
+`_s` comes packed with CLI commands tailored for WordPress theme development :
+
+- `composer lint:wpcs` : checks all PHP files against [PHP Coding Standards](https://developer.wordpress.org/coding-standards/wordpress-coding-standards/php/).
+- `composer lint:php` : checks all PHP files for syntax errors.
+- `composer make-pot` : generates a .pot file in the `languages/` directory.
+- `npm run compile:css` : compiles SASS files to css.
+- `npm run compile:rtl` : generates an RTL stylesheet.
+- `npm run watch` : watches all SASS files and recompiles them to css when they change.
+- `npm run lint:scss` : checks all SASS files against [CSS Coding Standards](https://developer.wordpress.org/coding-standards/wordpress-coding-standards/css/).
+- `npm run lint:js` : checks all JavaScript files against [JavaScript Coding Standards](https://developer.wordpress.org/coding-standards/wordpress-coding-standards/javascript/).
+- `npm run bundle` : generates a .zip archive for distribution, excluding development and system files.
+
+Now you're ready to go! The next step is easy to say, but harder to do: make an awesome WordPress theme. :)
---
+++ index.js
@@ -0,0 +1,97 @@
+"use strict";
+
+const prompt = require("prompts");
+const download = require("download");
+const unzipper = require("unzipper");
+const etl = require("etl");
+const slugify = require("slugify");
+const path = require("path");
+const fs = require("fs");
+
+let url = "https://github.com/Automattic/_s/archive/refs/heads/master.zip";
+
+(async function () {
+  const questions = [
+    {
+      type: "text",
+      name: "theme_name",
+      message: `What's your theme name?`,
+      initial: `My Theme`,
+    },
+    {
+      type: "text",
+      name: "theme_slug",
+      message: `What's the theme slug?`,
+      initial: (prev) => slugify(prev, { replacement: "-", lower: true }),
+    },
+    {
+      type: "text",
+      name: "root_dir",
+      message: "Where do you want to save the theme?",
+      initial: (prev) => prev,
+      validate: (value) =>
+        fs.existsSync(value) ? `Sorry, this directory already exists.` : true,
+    },
+  ];
+
+  const { root_dir, theme_name, theme_slug } = await prompt(questions, {
+    onCancel: () => process.exit(),
+  });
+
+  const function_names =
+    slugify(theme_name, { replacement: "_", lower: true }) + "_";
+  const doc_blocks = slugify(theme_name, { replacement: "_", lower: false });
+  const prefixes = theme_slug + "-";
+  const constants = function_names.toUpperCase();
+
+  download(url)
+    .pipe(unzipper.Parse())
+    .pipe(
+      etl.map(async (entry) => {
+        let entry_path = entry.path.replace("_s-master", root_dir);
+
+        if ("Directory" === entry.type) {
+          await fs.promises.mkdir(entry_path);
+        } else {
+          let content = await entry.buffer();
+          content = content.toString();
+
+          // 1 - Search for '_s' (inside single quotations) to capture the text domain and replace with: 'megatherium-is-awesome'.
+          content = content.replace(/'_s'/g, `'${theme_slug}'`);
+
+          // 2 - Search for _s_ to capture all the functions names and replace with: megatherium_is_awesome_.
+          content = content.replace(/_s_/g, function_names);
+
+          // 3 - Search for Text Domain: _s in style.css and replace with: Text Domain: megatherium-is-awesome.
+          content = content.replace(
+            /Text Domain: _s/g,
+            "Text Domain: " + theme_slug
+          );
+
+          // 4 - Search for  _s (with a space before it) to capture DocBlocks and replace with:  Megatherium_is_Awesome.
+          content = content.replace(/ _s/g, " " + doc_blocks);
+
+          // 5 - Search for _s- to capture prefixed handles and replace with: megatherium-is-awesome-.
+          content = content.replace(/_s-/g, prefixes);
+
+          // 6 - Search for _S_ (in uppercase) to capture constants and replace with: MEGATHERIUM_IS_AWESOME_.
+          content = content.replace(/_S_/g, constants);
+
+          // Replace personal information
+          content = content.replace(
+            /Theme Name: _s/g,
+            "Theme Name: " + theme_name
+          );
+
+          // Rename _s.pot from languages folder to use the theme's slug
+          if (entry_path.endsWith("_s.pot")) {
+            entry_path = entry_path.replace("_s.pot", theme_slug + ".pot");
+          }
+
+          await fs.promises.writeFile(entry_path, content);
+        }
+
+        //   entry.autodrain();
+      })
+    );
+})();
